6b9455f4597d9e4be8237a07597f0729d2755ee9
Ved.Mulkalwar <ved.mulkalwar@ril.com>
2020-01-16 02:05:18 +0530

    fixed the comparison of threshold numbers and added better print statements

--- src/event_processing/event_processor.py
+++ src/event_processing/event_processor.py
@@ -29,8 +29,8 @@ class EventProcessor():
         # 1. FIND/Create the key of the list using COUNTRY:PINCODE:"ATTRIBUTE"
         # 2. LPUSH the timestamp of the event to the list
         # 3. Compare the rightmost element's ts with the current ts
-        #   3a. if > # mins  pop the rightmost element. Repeat step 3.
-        #   3b. else compare the lenghth of list with threshcount. If greater, raise an alert
+        #    if > # mins  pop the rightmost element. Repeat step 3.
+        # 4. Compare the lenghth of list with threshcount. If greater, raise an alert
         for key, val in msg.items():
             if key not in ["TS", "LAT", "LON", "COUNTRY", "PINCODE"] and val==1:
                 list_key = msg["COUNTRY"] + "_" + msg["PINCODE"] + "_" + key
@@ -49,6 +49,4 @@ class EventProcessor():
 
                 list_length = self.r.llen(list_key)
                 if list_length > self.epidemic_thresholds.get(key).get("count"):
-                    print("TODO: raise an alert")
-                else:
-                    print("Don't raise an alert. Move on. Chalte bano bhai")
+                    print("TODO: raise an alert. Its an epidemic. Save yourselves. Runn for the hill!")
